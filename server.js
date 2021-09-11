f592eab2a23d1156de350fab99707068f9254c6d
João Pedro <jpedross1999@gmail.com>
2021-09-11 20:00:45 -0300

    end feedback

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.get('/feedback', function (req, res) {
 	res.sendFile(path.join(__dirname, './build', 'index.html'))
 })
 
-app.get('*', function (req, res) {
+app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, './build', 'index.html'))
 })
 
