e7913c623414b1b9065d1c8ec8686f9c7ff31356
Mayra <mayra.cademartori@gmail.com>
2021-09-11 16:59:24 -0300

    Feat(background)

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -2,16 +2,14 @@ import React from 'react'
 import { Router, Switch, Route } from 'react-router-dom'
 import Path from '../constants/Path'
 import HistoryService from '../services/HIstoryService'
-import Login from './login'
 import NotFound from './not_found'
-import Register from './register'
 
 const Main: React.FC = () => {
 	return (
 		<Router history={HistoryService}>
 			<Switch>
-				<Route exact path={Path.LOGIN} component={Login} />
-				<Route exact path={Path.REGISTER} component={Register} />
+				<Route exact path={Path.LOGIN} component={() => <></>} />
+				<Route exact path={Path.REGISTER} component={() => <></>} />
 				<Route path={'/'} component={NotFound} />
 			</Switch>
 		</Router>
