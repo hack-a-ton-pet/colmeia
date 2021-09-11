e6b2abe76575226c145b13524d789575df875cc7
João Pedro <jpedross1999@gmail.com>
2021-09-11 19:55:14 -0300

    merge

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const email = process.env.EMAIL
 const password = process.env.PASSWORD
 const MAX_FEEDBACK_SIZE = 3000
 
+console.log(process.env.EMAIL)
+console.log(process.env.PASSWORD)
+
 const transporter = nodemailer.createTransport({
 	service: 'gmail',
 	auth: {
